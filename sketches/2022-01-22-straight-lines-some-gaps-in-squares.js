1e2659fc05fc12ec0cab7358d2e09f76d70fd86b
Trevor Litsey <trevorlitsey@gmail.com>
2022-01-22 23:47:01 -0600

    more!

--- sketches/2022-01-22-straight-lines-some-gaps-in-squares.js
+++ sketches/2022-01-22-straight-lines-some-gaps-in-squares.js
@@ -9,8 +9,10 @@ const settings = {
   units: 'px',
 };
 
-const PAGE_MARGIN = 50;
-const HOW_MANY_LINES = 100; // plus one :)
+const SQUARE_WIDTH = 200;
+const SQUARE_MARGIN = 75;
+const PAGE_MARGIN = 200;
+const HOW_MANY_LINES = 10; // plus one :)
 const GAP_SIZE = 50;
 
 const makeLines = ([fromX, fromY], width, height) => {
@@ -20,7 +22,7 @@ const makeLines = ([fromX, fromY], width, height) => {
     const x = fromX + i * (width / HOW_MANY_LINES);
     let y = Math.random() > 0.5 ? fromY : fromY + GAP_SIZE * Math.random();
     while (y < fromY + height) {
-      const newY = y + (height * Math.random()) / 10;
+      const newY = y + height * Math.random();
       lines.push([
         [x, y],
         [x, newY >= fromY + height ? fromY + height : newY],
@@ -35,13 +37,28 @@ const makeLines = ([fromX, fromY], width, height) => {
 const sketch = ({ trimWidth: width, trimHeight: height }) => {
   const lines = [];
 
-  lines.push(
-    ...makeLines(
-      [0 + PAGE_MARGIN, 0 + PAGE_MARGIN],
-      width - PAGE_MARGIN * 2,
-      height - PAGE_MARGIN * 2
-    )
-  );
+  const howManyAcross = Math.floor((width - PAGE_MARGIN * 2) / SQUARE_WIDTH);
+  const howManyDown = Math.floor((height - PAGE_MARGIN * 2) / SQUARE_WIDTH);
+
+  const startX =
+    (width - SQUARE_WIDTH * howManyAcross - SQUARE_MARGIN * howManyAcross) / 2;
+  const startY =
+    (height - SQUARE_WIDTH * howManyDown - SQUARE_MARGIN * howManyDown) / 2;
+
+  for (let x = 0; x < howManyAcross; x++) {
+    for (let y = 0; y < howManyDown; y++) {
+      lines.push(
+        ...makeLines(
+          [
+            startX + x * SQUARE_WIDTH + x * SQUARE_MARGIN,
+            startY + y * SQUARE_WIDTH + y * SQUARE_MARGIN,
+          ],
+          SQUARE_WIDTH,
+          SQUARE_WIDTH
+        )
+      );
+    }
+  }
 
   return (props) => renderPaths(lines, props);
 };
